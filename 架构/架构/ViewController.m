78299238c8c5f79279f4a6252847a81bed3dbdce
Bonway <ibonway@aliyun.com>
2019-01-28 10:55:05 +0800

    初始化 项目工程

---
+++ 架构/架构/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  架构
+//
+//  Created by Bonway on 2019/1/28.
+//  Copyright © 2019 Bonway. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ 架构/架构/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  架构
+//
+//  Created by Bonway on 2019/1/28.
+//  Copyright © 2019 Bonway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
